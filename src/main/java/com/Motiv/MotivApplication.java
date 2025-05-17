b55bd6831058d32d6a408b1a939f6190c531505d
”EvanO3” <evan.oni@hotmail.com>
2025-05-17 19:36:29 -0400

    first commit

---
+++ src/main/java/com/Motiv/MotivApplication.java
@@ -0,0 +1,13 @@
+package com.Motiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Motiv/MotivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Motiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
