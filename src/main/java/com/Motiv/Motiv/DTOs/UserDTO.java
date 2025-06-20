df1a9c40fd742f1a0d9e991edd4d52ef00af9266
”EvanO3” <evan.oni@hotmail.com>
2025-06-19 20:06:58 -0400

    DTO folder

---
+++ src/main/java/com/Motiv/Motiv/DTOs/UserDTO.java
@@ -0,0 +1,62 @@
+package com.Motiv.Motiv.DTOs;
+
+import java.util.UUID;
+
+public class UserDTO {
+    
+
+    private String name;
+    private double weight;
+    private int height;
+    private int age;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+
+    public UserDTO(String name, double weight, int height, int age){
+        this.name= name;
+        this.weight=weight;
+        this.height=height;
+        this.age= age;
+
+
+    }
+
+
+    public int getAge(){
+        return age;
+    }
+
+public void setAge(int age){
+    this.age= age;
+}
+
+public double getWeight(){
+    return weight;
+
+}
+}
