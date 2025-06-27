3121aecfde44cc8bc4c3ef20711cfc6a1bb5998d
”EvanO3” <evan.oni@hotmail.com>
2025-06-26 21:55:04 -0400

    made rest controller bean

--- src/main/java/com/Motiv/Motiv/Configs/RestTemplateConfig.java
+++ src/main/java/com/Motiv/Motiv/Configs/RestTemplateConfig.java
@@ -1,5 +1,7 @@
 package com.Motiv.Motiv.Configs;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,9 +9,11 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
    
+    @Value("${supabase.api_key}")
+    private String api;
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.defaultHeader("apikey", api).build();
     }
 
   
